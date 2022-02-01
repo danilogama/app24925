985406cfab744381f280807c22810ad83144e2dc
danilogama <danilogm7@gmail.com>
2022-02-01 15:57:12 -0300

    commit prueba

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Cambio de prueba desafio Clase 3
         </a>
       </header>
     </div>
