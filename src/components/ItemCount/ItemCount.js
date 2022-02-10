0cc2057874c5384fdce89a0e4849fd3cb53c2d35
danilogama <danilogm7@gmail.com>
2022-02-10 17:19:26 -0300

    agregando card

---
+++ src/components/ItemCount/ItemCount.js
@@ -0,0 +1,60 @@
+import { useState, useEffect } from 'react'
+import './ItemCount.css';
+
+const ItemCount = ({ stock, initial,  onAdd }) => {
+    const [count, setCount] = useState(0) 
+
+
+    useEffect(() => {
+        setCount(initial)
+
+        return () => {
+            setCount(0)          
+        }
+    }, [])
+
+    const increment = () => {
+        
+        if(count < stock){
+            setCount(count + 1)
+        }else{
+            console.log('No hay stock')
+        }
+    }
+
+    const decrement = () => {
+        if(count > initial) {
+            setCount(count - 1)
+        } 
+
+    }    
+    
+    return(
+        <>
+        <div className='item'>
+
+            <h3>Producto</h3>
+            
+            <button onClick={decrement}>-</button>
+            <h3>{count}</h3>
+            <button onClick={increment}>+</button>
+            <h3></h3>
+            <button onClick={()=>onAdd(count)}>Agregar al carrito</button>
+        </div>
+
+        <div class="card">
+            <img src="/images/ntbkLenovo.jpg" alt="ntbkLenovo"  height="200px"/>
+            <h1>Notebook Gaming HP</h1>
+            
+            <p class="price">$19.99</p>
+            <button onClick={decrement}>-</button>
+            <h3>{count}</h3>
+            <button onClick={increment}>+</button>
+            <p><button onClick={()=>onAdd(count)}>Agregar al carrito</button></p>
+        </div>
+        </>
+        
+    )
+}
+
+export default ItemCount
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,9 +1,16 @@
 import './ItemListContainer.css';
+import ItemCount from '../ItemCount/ItemCount';
 
 const ItemListContainer = (props) =>{
+
+    const addProduct = (count) =>{
+        console.log('Cantidad de productos: ' + count )
+    }
     return (
-        <h2>{props.greeting}</h2>    
-        )    
+        <><h2>{props.greeting}</h2>   
+        <ItemCount stock={5} initial={1} onAdd={addProduct}/> 
+        </>    
+    )    
   }
   
   export default ItemListContainer
