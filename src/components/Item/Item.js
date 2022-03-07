04152e024d506ab47c90c99cdcaff70fc4b90b46
danilogama <danilogm7@gmail.com>
2022-03-06 22:30:40 -0300

    firebase1

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,10 +1,7 @@
 import './Item.css';
 import { Link } from 'react-router-dom'
 
-export const products = [
-	{ id: 1 , title: 'Notebook Hp Stream 14"', stock: 5, category: 'notebook' , description: 'descriptiontext1', price: '354',pictureUrl:'https://http2.mlstatic.com/D_NQ_NP_603880-MLU48233986823_112021-F.webp'}
-]
-
+export const products = []
 
 export const getProducts =  new Promise ((resolve,reject) => {
 		setTimeout(() =>{
